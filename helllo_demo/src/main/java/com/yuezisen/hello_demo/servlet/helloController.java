16d8f8a8f8f7ec3b2f65a2949d8615825540f13a
yuezisen <2863590460@qq.com>
2020-07-03 14:36:25 +0800

    Second

--- helllo_demo/src/main/java/com/yuezisen/hello_demo/servlet/helloController.java
+++ helllo_demo/src/main/java/com/yuezisen/hello_demo/servlet/helloController.java
@@ -3,22 +3,13 @@ package com.yuezisen.hello_demo.servlet;
 import com.yuezisen.hello_demo.POJO.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.print.attribute.standard.PresentationDirection;
-import java.util.Map;
+@Controller public class helloController {
+    @Autowired Person person;
 
-@Controller
-public class helloController {
-    @Autowired
-    Person person;
-    @ResponseBody
-    @RequestMapping("/hello")
-    public ModelAndView hello (){
-        ModelAndView mv = new ModelAndView("index" , (Map<String, ?>) person);
-        return mv;
+    @ResponseBody @RequestMapping("/hello") public String hello() {
+        return "hello Springboot!";
     }
 }
